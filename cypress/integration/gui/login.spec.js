07694c224dc7b860841c325caf0c61a2c600c593
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-13 16:50:45 -0300

    Curso Cypress

---
+++ cypress/integration/gui/login.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Login', () => {
+    it('successfully', () => {
+      cy.login()
+  
+      cy.get('.qa-user-avatar').should('exist')
+    })
+  })
+  
